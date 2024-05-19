e0411fd04c375afcd26ae56ddf05e7862ee55ed4
TheophilusRuben <Theophilus.Ruben@selecthealth2.org>
2024-05-19 13:54:53 +0530

    Commit last

---
+++ TestProj/UnitTest1.cs
@@ -0,0 +1,16 @@
+//using Microsoft
+using Microsoft.Playwright.MSTest;
+namespace TestProj
+{
+	[TestClass]
+	public class UnitTest1:PageTest
+	{
+		[TestMethod]
+		public async Task TestMethod1()
+		{
+			await Page.GotoAsync("https://localhost:7257");
+			await Page.Locator("input[name=email]").FillAsync("Theo");
+			Assert.AreEqual("Theo",await Page.Locator("input[name=email]").InputValueAsync());
+		}
+	}
+}
